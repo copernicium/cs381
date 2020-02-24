6b5669f1933221c2322cd007f935741f8e200bd2
Logan Traffas <logan.traffas@gmail.com>
2020-02-23 18:59:59 -0800

    Add starter file with some preliminary ideas

---
+++ Lang.hs
@@ -0,0 +1,31 @@
+module Lang where -- TODO name language
+
+-- Abstract Syntax
+
+type Var = String
+
+data Expr
+   = LitI Int
+   | LitB Bool
+   | LitS String
+   | Add Expr Expr
+   | Declare Var Expr -- TODO figure out type system and how to track variables
+   | Bind Var Expr
+   | Ref Var
+  deriving (Eq,Show)
+
+data Stmt
+   = Set Expr
+   | IfElse Expr Stmt Stmt
+   | While Expr [Stmt]
+   | Begin [Stmt]
+  deriving (Eq,Show)
+
+defaultI :: Expr
+defaultI = LitI 0
+
+-- Good Examples
+
+-- int x = 0
+ex1 :: Stmt
+ex1 = undefined -- TODO
